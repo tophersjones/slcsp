0c40e1973db823dbc5ce49d72a592419baeb00f7
Chris <tophersjones@gmail.com>
2018-08-08 11:03:36 -0500

    final

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,34 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-/**
- * COMPONENT
- */
-export const UserHome = props => {
-  const {email} = props
+class UserHome extends Component {
+  constructor () {
+    super() 
+    this.state = {
+      zips: [],
+      info: []
+    }
+    this.componentDidMount = this.componentDidMount.bind(this)
+  }
 
+  componentDidMount () {
+    // axios.get('/api/slcsp').then(res => res.data).then(zips => {
+    //   console.log(zips)
+    //   this.setState({
+    //     zips: zips
+    //   })
+    // })
+  }
+
+  render () { 
+    // const zips = this.state.zips
+    // console.log('zips2', zips)
+    // zips.map(zip => {
+    //   const zipcode = Number(zip.zipcode)
+    //   console.log('zip', zipcode)
+      // axios.put('/api/zips', zipcode).then(res => {
+      //   console.log(res)
+      //   return res.data 
+      // }).then(info => {
+      //   console.log('info', info)
+        // this.setState({
+        //   info: info
+        // })
+      // })
+    // })
   return (
     <div>
-      <h3>Welcome, {email}</h3>
+        <h3>Welcome, there</h3>
     </div>
   )
 }
-
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    email: state.user.email
-  }
 }
 
-export default connect(mapState)(UserHome)
-
-/**
- * PROP TYPES
- */
-UserHome.propTypes = {
-  email: PropTypes.string
-}
+export default UserHome
